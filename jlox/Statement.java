8dbd8e2b615da15bc07fd3d2cebc8a2b1f698c82
pya-h <p4ya.py@gmail.com>
2026-01-26 06:59:00 +0330

    feat: improve my class template generator - Now it also can generate a class with any number of childs - Generate basic template for Statement class

---
+++ jlox/Statement.java
@@ -0,0 +1,36 @@
+package jlox;
+
+public abstract class Statement {
+	public interface Visitor<T> {
+		T visitExpressionStatement(ExpressionStatement statement);
+		T visitPrintStatement(PrintStatement statement);
+	}
+
+	public static class ExpressionStatement extends Statement {
+		final Expression expression;
+
+		public ExpressionStatement(Expression expression) {
+			this.expression = expression;
+		}
+
+		@Override<T>
+		T accept(Visitor<T> visitor) {
+			return visitor.visitExpressionStatement(this);
+		}
+	}
+
+	public static class PrintStatement extends Statement {
+		final Expression expression;
+
+		public PrintStatement(Expression expression) {
+			this.expression = expression;
+		}
+
+		@Override<T>
+		T accept(Visitor<T> visitor) {
+			return visitor.visitPrintStatement(this);
+		}
+	}
+
+	abstract <T> T accept(Visitor<T> visitor);
+}
--- jlox/tools/FamilyGenerator.java
+++ jlox/tools/FamilyGenerator.java
@@ -8,21 +8,52 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class ExpressionAstGenerator {
-    public static void main(String[] args) throws IOException, Exception {
-        if (args.length != 2) {
-            final String name = new Object() {
-            }.getClass().getEnclosingClass().getSimpleName();
-            System.out.println("[Usage] jlox.tools." + name + " " + "<output-directory> <base-class>");
-            System.exit(64);
-        }
-        final String outDir = args[0], baseClass = args[1];
-        final Map<String, List<String>> childs = new HashMap<>();
+public class FamilyGenerator {
+    public static void prepareExpressionChildsParameters(Map<String, List<String>> childs, String baseClass) {
+        childs.clear();
         childs.put("Binary", Arrays.asList(baseClass + " left", "Token operator", baseClass + " right"));
         childs.put("Unary", Arrays.asList("Token operator", baseClass + " right"));
         childs.put("Literal", Arrays.asList("Object value"));
-        childs.put("Grouping", Arrays.asList(baseClass + " expression"));
-        generate(outDir, baseClass, childs);
+        childs.put("Grouping", Arrays.asList(baseClass + " expression"));        
+    }
+
+    public static void prepareStatementChildsParameters(Map<String, List<String>> childs, String baseClass, String... childNames) {
+        childs.clear();
+        for(String child: childNames) {
+            childs.put(child + baseClass, Arrays.asList("Expression expression"));
+        }
+    }
+
+    public static void main(String[] args) throws IOException, Exception {
+        if (args.length < 3) {
+            final String name = new Object() {
+            }.getClass().getEnclosingClass().getSimpleName();
+            System.err.println("[Usage] jlox.tools." + name + " " + "<generation-mode> <output-directory> <base-class> [<child1> <child2> ...]");
+            System.exit(64);
+        }
+        final String outDir = args[1];
+        final Map<String, List<String>> childs = new HashMap<>();
+
+        switch(args[0].toLowerCase()) {
+            case "expr":
+                prepareExpressionChildsParameters(childs, args[2]);
+                generate(outDir, args[2], childs);
+                break;
+            case "stmt":
+                prepareStatementChildsParameters(childs, args[2], Arrays.stream(args).skip(3).toArray(String[]::new));
+                generate(outDir, args[2], childs);
+                break;
+            case "both":
+                prepareExpressionChildsParameters(childs, args[2]);
+                generate(outDir, args[2], childs);
+                prepareStatementChildsParameters(childs, args[3], Arrays.stream(args).skip(4).toArray(String[]::new));
+                generate(outDir, args[3], childs);
+                break;
+            default:
+                System.err.println("Invalid mode! modes: expr, stmt, both");
+                System.exit(64);
+        }
+
     }
 
     public static void generate(final String outputDirectory, final String baseClass,
@@ -61,18 +92,22 @@ public class ExpressionAstGenerator {
             writer.println("\n" + constructorDecleration + " {" + constructorBody + subClassTabs + "}\n");
 
             writer.println(tabs + "\t@Override<T>\n" + tabs + "\tT accept(Visitor<T> visitor) {");
-            writer.println(tabs + "\t\treturn visitor.visit" + className + baseClass + "(this);\n" + tabs + "\t}");
+            writer.println(tabs + "\t\treturn visitor.visit" + getVisitorName(className, baseClass) + "(this);\n" + tabs + "\t}");
             writer.println(tabs + "}\n");
         }
         writer.println("\tabstract <T> T accept(Visitor<T> visitor);\n}");
         writer.close();
     }
 
+    public static String getVisitorName(final String className, final String baseClass) {
+        return className.toLowerCase().contains(baseClass.toLowerCase()) ? className : className + baseClass; // prevent unnecessary repeats
+    }
+
     public static void defineVisitor(PrintWriter writer, final String baseClass, final Set<String> subClasses) {
         writer.println("\tpublic interface Visitor<T> {");
 
         for(String cls: subClasses) {
-            writer.println("\t\tT visit" + cls + baseClass + "(" + cls + " " + baseClass.toLowerCase() + ");");
+            writer.println("\t\tT visit" + getVisitorName(cls, baseClass) + "(" + cls + " " + baseClass.toLowerCase() + ");");
         }
         writer.println("\t}\n");
     }
